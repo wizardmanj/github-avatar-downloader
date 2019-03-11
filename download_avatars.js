3869cd9c60840aa55f7d871bb5ae6fd32d5be3cf
Josh Whelan <josh.d.whelan@gmail.com>
2019-03-11 23:28:14 +0000

    added require: token; referencing authorization token in hidden file

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,24 @@
+var token = require('./secrets');
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': token.GITHUB_TOKEN
+      }
+    };
+  
+    request(options, function(err, res, body) {
+      cb(err, body);
+    });
+  }
+
+ getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+  });
