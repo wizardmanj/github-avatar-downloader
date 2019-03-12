3e2e5fe5b7d36b6bced7747752d3bf2b0b1f0d70
Josh Whelan <josh.d.whelan@gmail.com>
2019-03-12 00:48:44 +0000

    connected functions and succesffully downloaded requisite files

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var token = require('./secrets');
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -14,15 +15,28 @@ function getRepoContributors(repoOwner, repoName, cb) {
     };
   
     request(options, function(err, res, body) {
-      cb(err, body) 
-        var parsedBody = JSON.parse(body);
-        for (var url of parsedBody) {
-            console.log(url['avatar_url']);
+        var users = JSON.parse(body);
+        for (var obj of users) {
+            cb(obj["avatar_url"], `avatars/${obj.login}.jpg`);
         }
     });
   }
 
- getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
-    console.log("Result:", result);
-  });
+ getRepoContributors("jquery", "jquery", downloadImageByURL);
+
+  function downloadImageByURL(url, filePath) {
+    request.get(url)
+       .on('error', function (err) {                                   
+         throw err; 
+       })
+       .on('response', function (response) {                          
+        console.log('Downloading image...');
+        console.log('Response Status Code: ', response.statusCode);
+        console.log('Response Status Message: ', response.statusMessage);
+        console.log('Response Headers: ', response.headers['content-type']);
+       })
+       .on('end', function() {
+        console.log('Download complete.')})
+       .pipe(fs.createWriteStream(filePath));               
+  }
+
